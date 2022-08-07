293cb585e6a91265a66bdfcf63945ba63c209e1f
Serzhan <serjan_kosmos@mail.ru>
2022-08-07 14:01:47 +0600

    Complete

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 # app.py
 
 from flask import Flask, request
-from flask_restx import Api, Resource
+from flask_restx import Api, Resource, namespace
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema, fields
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -18,11 +19,12 @@ class Movie(db.Model):
     description = db.Column(db.String(255))
     trailer = db.Column(db.String(255))
     year = db.Column(db.Integer)
-    rating = db.Column(db.Float)
+    rating = db.Column(db.Integer)
     genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
     genre = db.relationship("Genre")
     director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
     director = db.relationship("Director")
+
 
 class Director(db.Model):
     __tablename__ = 'director'
@@ -35,6 +37,143 @@ class Genre(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
 
+
+class MovieSchema(Schema):
+    id = fields.Int(dump_only=True)
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Int()
+    genre_id = fields.Int()
+    director_id = fields.Int()
+
+
+class DirectorSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
+
+class GenreSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
+
+movie_schema = MovieSchema()
+movies_schema = MovieSchema(many=True)
+director_schema = DirectorSchema()
+directors_schema = DirectorSchema(many=True)
+genre_schema = GenreSchema()
+genres_schema = GenreSchema(many=True)
+
+
+api = Api(app)
+movie_ns = api.namespace('movies')
+director_ns = api.namespace('directors')
+genre_ns = api.namespace('genres')
+
+
+@movie_ns.route('/')
+class MoviesView(Resource):
+    def get(self):
+        request_movies = Movie.query
+        args = request.args
+
+        director_id = args.get('director_id')
+        if director_id is not None:
+            request_movies = request_movies.filter(Movie.director_id == director_id)
+
+        genre_id = args.get('genre_id')
+        if genre_id is not None:
+            request_movies = request_movies.filter(Movie.genre_id == genre_id)
+        movies = request_movies.all()
+        return movies_schema.dump(movies), 200
+
+
+@movie_ns.route('/<int:mid>')
+class MoviesView(Resource):
+    def get(self, mid: int):
+        try:
+            movie_by_mid = Movie.query.get(mid)
+            return movie_schema.dumps(movie_by_mid), 200
+        except Exception as e:
+            return "", 404
+
+
+@director_ns.route('/')
+class DirectorView(Resource):
+    def get(self):
+        all_director = Director.query.all()
+        return directors_schema.dumps(all_director), 200
+
+
+@director_ns.route('/<int:did>')
+class DirectorView(Resource):
+    def get(self, did: int):
+        try:
+            director_by_did = Director.query.get(did)
+            return director_schema.dumps(director_by_did), 200
+        except Exception as e:
+            return "", 404
+
+    def post(self):
+        req_json = request.json
+        new_director = Director(**req_json)
+        with db.session.begin():
+            db.session.add(new_director)
+        return "", 201
+
+    def put(self, did):
+        director = Director.query.get(did)
+        req_json = request.json
+        director.name = req_json.get("name")
+        db.session.add(director)
+        db.session.commit()
+        return "", 204
+
+    def delete(self, did: int):
+        director = Director.query.get(did)
+        db.session.delete(director)
+        db.session.commit()
+        return "", 204
+
+
+@genre_ns.route('/')
+class GenreView(Resource):
+    def get(self):
+        all_genres = Genre.query.all()
+        return genres_schema.dumps(all_genres), 200
+
+
+@genre_ns.route('/<int:gid>')
+class GenreView(Resource):
+    def get(self, gid: int):
+        try:
+            genre_by_did = Genre.query.get(gid)
+            return genre_schema.dumps(genre_by_did), 200
+        except Exception as e:
+            return "", 404
+
+    def post(self):
+        req_json = request.json
+        new_director = Director(**req_json)
+        with db.session.begin():
+            db.session.add(new_director)
+        return "", 201
+
+    def put(self, gid):
+        genre = Genre.query.get(gid)
+        req_json = request.json
+        genre.name = req_json.get("name")
+        db.session.add(genre)
+        db.session.commit()
+        return "", 204
+
+    def delete(self, gid: int):
+        genre = Genre.query.get(gid)
+        db.session.delete(genre)
+        db.session.commit()
+        return "", 204
 
 
 if __name__ == '__main__':
